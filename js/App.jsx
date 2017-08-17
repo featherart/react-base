e92d6d98eec12125e5836fe943a1097ed3dd60f8
Feather Knee <featherknee@gmail.com>
2017-08-17 15:11:31 -0700

    Derptyderp.

--- js/App.jsx
+++ js/App.jsx
@@ -10,7 +10,7 @@ const App = () =>
   <div className="wrapper">
     <Navbar />
     <div className="app">
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route path="/two" component={PageTwo} />
     </div>
   </div>;
